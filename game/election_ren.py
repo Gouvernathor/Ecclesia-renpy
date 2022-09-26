c48f85a98637919f3a9f0ce8a20aad51a0e1bc8d
Asriel Senna <aanonyme.personne@hotmail.fr>
2022-09-26 12:06:07 +0200

    implement Sortition

--- game/election_ren.py
+++ game/election_ren.py
@@ -438,5 +438,23 @@ class ElectionMethod(namedtuple("ElectionMethod", ("voting_method", "attribution
     def election(self, *args, **kwargs):
         return self.attribution_method.attrib(self.voting_method.vote(*args, **kwargs))
 
-# faire un ElectionMethod qui implémente le tirage au sort parmi la population
-# sans passer par les partis
+class Sortition(python_object):
+    """
+    Implements a selection by lottery, directly among the population.
+    Poses problems that SingleVote+Randomize doesn't, as it does not return parties.
+    That's not supported by the current House system.
+    """
+    __slots__ = ("nseats", "randomobj")
+
+    def __init__(self, nseats, *, randomkey=None, randomobj=None):
+        self.nseats = nseats
+        if None not in (randomobj, randomkey):
+            raise TypeError("Only one of randomobj and randomkey must be provided.")
+        if randomobj is None:
+            randomobj = renpy.random.Random(randomkey)
+        self.randomobj = randomobj
+
+    def election(self, pool):
+        return {(c, 1) for c in self.randomobj.sample(pool, self.nseats)}
+
+ElectionMethod.register(Sortition)
