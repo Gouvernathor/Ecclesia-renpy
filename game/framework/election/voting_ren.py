bc564541fcfe622b14ff4fc9ff09da89c387e621
Asriel Senna <aanonyme.personne@hotmail.fr>
2023-05-20 04:52:19 +0200

    Implement CardinalVote and ApprovalVote

--- game/framework/election/voting_ren.py
+++ game/framework/election/voting_ren.py
@@ -7,6 +7,7 @@ init python in voting_method:
 _constant = True
 
 import abc
+from math import nextafter, floor
 import store
 from store import results_format
 
@@ -86,18 +87,76 @@ class OrderingVote(VotingMethod):
 class CardinalVote(VotingMethod):
     """
     Each voter gives a note (or grade) for each of the candidates.
+
+    This one is not as straightforward as the two preceding ones, even setting aside strategic voting.
+    What do you consider the range of grades to cover ? From nazis to angels, or from the worst present candidate to the best ?
+    The answer lies only in the minds of the voters.
+    The latter is more akin to OrderingVote, so I made the former the default,
+    but it causes issues for lower grades so ApprovalVote uses the latter.
     """
     __slots__ = ("grades") # the number of different grades, >1
     return_format = results_format.SCORES
     name = _("Score Vote")
 
-class ApprovalVote(CardinalVote):
+    def __init__(self, grades):
+        super().__init__()
+        self.grades = grades
+
+    def vote(self, pool):
+        """
+        Each voter gives a grade to each party proportional to the raw disagreement.
+        This may yield situations where every party is graded 0, especially with lower numbers of grades.
+        """
+        grades = self.grades
+        rv = results_format.SCORES.fromgrades(grades)
+        partees = list(store.partis)
+        store.electrobj.shuffle(partees)
+
+        # uncorrected version, may return cases where every party is graded 0
+        grades = nextafter(grades, .0) # if the disagreement is .0, the grade will be grades-1 and not grades
+        for voter in pool:
+            for parti in partees:
+                grad = floor((1-(voter^parti)) * grades)
+                rv[parti][grad] += 1
+
+        return rv
+
+class BalancedCardinalVote(CardinalVote, final=False):
+    def vote(self, pool):
+        """
+        Each voter gives a grade to each party, affine wrt its minimum and
+        maximum disagreement with the candidate parties only.
+        Each voter will give the best grade at least once, and
+        either grade all parties equally or give the worst grade at least once.
+        """
+        grades = self.grades
+        rv = results_format.SCORES.fromgrades(grades)
+        partees = list(store.partis)
+        store.electrobj.shuffle(partees)
+
+        # balanced version
+        grades = nextafter(grades, .0) # if the disagreement is .0, the grade will be grades-1 and not grades
+        for voter in pool:
+            prefs = {parti:1-(voter^parti) for parti in partees}
+            minpref = min(prefs.values())
+            maxpref = max(prefs.values())
+
+            if minpref != maxpref: # avoid division by zero
+                maxpref -= minpref
+
+            for parti in partees:
+                grad = floor(grades * (prefs[parti] - minpref) / maxpref)
+                rv[parti][grad] += 1
+
+        return rv
+
+class ApprovalVote(BalancedCardinalVote):
     """
     Each voter approves, or not, each of the candidates.
 
     Technically a special case of grading vote where grades are 0 and 1,
     but it makes it open to additional attribution methods (proportional ones for instance).
-    The format it returns data in, however, is not the same as CardinalVote.
+    The format it returns, however, is not the same as CardinalVote.
     """
     __slots__ = ()
     return_format = results_format.SIMPLE
@@ -105,3 +164,10 @@ class ApprovalVote(CardinalVote):
 
     def __init__(self):
         super().__init__(grades=2)
+
+    def vote(self, pool):
+        scores = super().vote(pool)
+        rv = self.return_format()
+        for parti, (_nos, yeas) in scores.items():
+            rv[parti] += yeas
+        return rv
