505fcbde81c5c6ac345706974bad2e512ae69372
Asriel Senna <aanonyme.personne@hotmail.fr>
2023-05-20 03:38:58 +0200

    cosmetic

--- game/framework/election/results_ren.py
+++ game/framework/election/results_ren.py
@@ -5,9 +5,9 @@ init -1 python in results_format:
 """
 _constant = True
 
-from collections import Counter as _Counter
+from collections import Counter as __Counter
 
-# SIMPLE : dict(parti : nombre de voix)
+# SIMPLE : Counter(parti : nombre de voix)
 #       {PS : 5, LR : 7} -> 5 voix pour le PS, 7 pour LR
 # ORDER : iterable(iterable(partis ordonnés par préférence décroissante))
 #       [(LR, PS, LFI), (LFI, PS,), ] -> un électeur préfère LR puis PS puis LFI,
@@ -18,8 +18,9 @@ from collections import Counter as _Counter
 # SCORES : dict(parti : iterable(nombre de voix pour chaque note))
 #       {PS : (0, 2, 5, 9, 1)} -> le PS a reçu 0 fois la pire note, 1 fois la meilleure et t'as compris
 #       (len(tup) for tup in result.values()) est constant, égal à votingmethod.grades
+#       si l'électeur doit noter tous les candidats, (sum(tup) for tup in result.values()) est constant et égal au nombre d'électeurs
 
-class SIMPLE(_Counter):
+class SIMPLE(__Counter):
     __slots__ = ()
 
     @classmethod
@@ -28,6 +29,7 @@ class SIMPLE(_Counter):
 
 class ORDER(tuple):
     __slots__ = ()
+
 class SCORES(python_dict):
     __slots__ = ()
 
